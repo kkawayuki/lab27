2a1879484e781d2053e32f1754193abb7594f570
kkawayuki <kyukiokawa@gmail.com>
2024-11-03 13:41:24 -0800

    Commit 4

--- main.cpp
+++ main.cpp
@@ -9,18 +9,41 @@ using namespace std;
 // prototypes
 void addVillager();
 int printMenu();
-void printVillager( map<string, tuple<int, string, string>>);
+void printVillagers(map<string, tuple<int, string, string>>);
 
-    int main()
+int main()
 {
     // map declaration
     map<string, tuple<int, string, string>> vInfo; // each pair is comprised of a string name(key), struct containing tuple of int/string/string
 
     vInfo["Test"] = make_tuple(1, "a", "a"); // test
 
+    do
+    {
+        switch (printMenu())
+        {
+        case(1):
+            printVillagers(vInfo);
+            break;
 
-    printMenu();
+        case(2):
+            printVillagers(vInfo);
+            break;
 
+        case(3):
+            break;
+
+        case(4):
+            break;
+
+        case(5):
+            break;
+        
+        default:
+            break;
+        }
+    }
+    while(printMenu() != 6);
 
     return 0;
 }
@@ -32,21 +55,27 @@ void addVillager()
 int printMenu()
 {
     int c;
-    cout << "1. Add Villager\n2. Delete Villager\n3. Increase Friendship\n4. Decrease Friendship\n5. Search for Villager\n6. Exit\n\n"; 
+    cout << "1. Add Villager\n2. Delete Villager\n3. Increase Friendship\n4. Decrease Friendship\n5. Search for Villager\n6. Exit\n\n";
     cout << "Choice: ";
     cin >> c;
-    while(c < 1 || c > 6)
+    while (c < 1 || c > 6)
     {
         cout << "ERROR: Choice must be a number 1-6, try again: ";
         cin >> c;
     }
-    return(c); 
+    return (c);
 }
 
-void printVillager(map<string, tuple<int, string, string>>villagers)
+void printVillagers(map<string, tuple<int, string, string>> villagers)
 {
-    for(const auto &a : villagers) //for each villager
+    if (villagers.size() > 0)
     {
-        cout << villagers.
+        cout << "Villager Details: \n";
+        for (const auto &a : villagers) // for each villager
+            cout << a.first << " [" << "Friendship Level: " << get<0>(a.second) << ", Species: " << get<1>(a.second) << ", Catchphrase: " << get<2>(a.second) << "]\n"; //access specific tuple elements
+    }
+    else
+    {
+        cout << "No Villagers yet, try adding some first!\n"; 
     }
 }
